f0f484cfde09c17dcf54096c3fbca2c5a852f0dd
anfelab <57996654+anfelab@users.noreply.github.com>
2021-06-21 00:22:06 +0900

    Initial Commit

---
+++ testproject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,81 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ENV_M01_ATMOSPHERE_HEAVY = 4224644313U;
+        static const AkUniqueID PLAY_ENV_M01_BASE_BIRDS = 467916215U;
+        static const AkUniqueID PLAY_ENV_M01_BASE_CAVE = 3637765342U;
+        static const AkUniqueID PLAY_ENV_M01_POS_BIRDS = 2676917488U;
+        static const AkUniqueID PLAY_ENV_M01_POS_INSECTS = 763318039U;
+        static const AkUniqueID PLAY_ENV_M01_POS_LAKE = 3204301025U;
+        static const AkUniqueID PLAY_ENV_M01_POS_LEAVES_RUSTLE = 846114944U;
+        static const AkUniqueID PLAY_ENV_M01_POS_LEAVES_RUSTLE_NOWIND = 341662512U;
+        static const AkUniqueID PLAY_ENV_M01_POS_POND = 3230230181U;
+        static const AkUniqueID PLAY_ENV_M01_POS_RIVER = 2137656390U;
+        static const AkUniqueID PLAY_ENV_M01_POS_SEA_WAVES_CLOSE = 2269971331U;
+        static const AkUniqueID PLAY_ENV_M01_POS_SEA_WAVES_MEDIUM = 2466719636U;
+        static const AkUniqueID PLAY_ENV_M01_POS_SEAGULLS = 4235748092U;
+        static const AkUniqueID PLAY_ENV_M01_POS_SEASHORE = 2039408100U;
+        static const AkUniqueID PLAY_ENV_M01_POS_SHIMMER = 2340908993U;
+        static const AkUniqueID PLAY_ENV_M01_POS_STEADY_WATER = 1776298094U;
+        static const AkUniqueID PLAY_ENV_M01_POS_WATERFALL = 307210770U;
+        static const AkUniqueID PLAY_ENV_M01_POS_WATERFALL_SMALL = 1122186766U;
+        static const AkUniqueID PLAY_ENV_M01_POS_WIND = 3099826098U;
+        static const AkUniqueID PLAY_ENV_M01_POS_WIND_TREES = 1172726548U;
+        static const AkUniqueID PLAY_PL_MOT_FOLEY_MVT = 497670973U;
+        static const AkUniqueID PLAY_PL_MOT_FOLEY_MVT_UNDERWATER = 3720299939U;
+        static const AkUniqueID PLAY_PL_MOT_FS_BARE = 1590791414U;
+        static const AkUniqueID PLAY_PL_MOT_FS_BARE_JUMP = 2535771169U;
+        static const AkUniqueID PLAY_PL_MOT_FS_BARE_LAND = 3676832638U;
+        static const AkUniqueID PLAY_PL_MOT_FS_CONCRETE = 3080637483U;
+        static const AkUniqueID PLAY_PL_MOT_FS_CONCRETE_JUMP = 1306850134U;
+        static const AkUniqueID PLAY_PL_MOT_FS_CONCRETE_LAND = 3669691297U;
+        static const AkUniqueID PLAY_PL_MOT_FS_DIRT = 3974941501U;
+        static const AkUniqueID PLAY_PL_MOT_FS_GRASS = 654160592U;
+        static const AkUniqueID PLAY_PL_MOT_FS_GRASS_JUMP = 3111818663U;
+        static const AkUniqueID PLAY_PL_MOT_FS_GRASS_LAND = 749080668U;
+        static const AkUniqueID PLAY_PL_MOT_FS_LEAVES = 1761647722U;
+        static const AkUniqueID PLAY_PL_MOT_FS_SAND = 2505848860U;
+        static const AkUniqueID PLAY_PL_MOT_FS_SAND_JUMP = 2274938739U;
+        static const AkUniqueID PLAY_PL_MOT_FS_SAND_LAND = 1637205776U;
+        static const AkUniqueID PLAY_PL_MOT_FS_SNOW = 3061334687U;
+        static const AkUniqueID PLAY_PL_MOT_FS_SNOW_LAND = 3893508365U;
+        static const AkUniqueID PLAY_PL_MOT_FS_WATER = 3054663551U;
+        static const AkUniqueID PLAY_PL_MOT_FS_WATER_JUMP = 1906010698U;
+        static const AkUniqueID PLAY_PL_MOT_FS_WATER_LAND = 2543743405U;
+        static const AkUniqueID PLAY_PL_MOT_HIT_BUSH = 1394695102U;
+        static const AkUniqueID PLAY_PL_MOT_HIT_LEAVES = 2012792292U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ENV_M01 = 792818075U;
+        static const AkUniqueID PL_MOT = 3424941384U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
